fac14ce2d3ffbcdfd476d7b2283012eab6e3b7f9
Sina Razavi <you@example.com>
2020-12-02 02:39:28 +0330

    Add project files.

---
+++ Domain/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Game
+    {
+        public Game()
+        {
+            Board = new Board();
+        }
+        public Board Board { get; set; }
+    }
+}
